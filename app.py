d6614724d2252095d6350dac78f6408e8088e3b8
durklanez <durkjosue@gmail.com>
2026-01-12 15:43:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
-from flask import Flask, jsonify, send_from_directory
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Rota do frontend
 @app.route("/")
-def index():
-    return send_from_directory(".", "index.html")
+def home():
+    return render_template("index.html")
 
-# API de teste
-@app.route("/api/ping")
-def ping():
-    return jsonify({
-        "status": "ok",
-        "message": "🔥 Backend Python ativo – Inteligência do Cafunfo online!"
-    })
-
-# Porta obrigatória do Render
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
+
