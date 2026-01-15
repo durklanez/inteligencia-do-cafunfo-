b56ebb551e5d719ce703bd41fceb84bcdb186048
durklanez <durkjosue@gmail.com>
2026-01-15 17:41:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+@app.route("/linguagens")
+def linguagens():
+
 
