a4d9f967ac1dc016a6c22de321dd5a435954dd7e
durklanez <durkjosue@gmail.com>
2026-01-15 17:27:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,21 +10,5 @@ def index():
 def linguagens():
     return render_template("linguagens.html")
 
-@app.route("/python")
-def python():
-    return render_template("python.html")
-
-@app.route("/javascript")
-def javascript():
-    return render_template("javascript.html")
-
-@app.route("/flutter")
-def flutter():
-    return render_template("flutter.html")
-
-@app.route("/bots")
-def bots():
-    return render_template("bots.html")
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
