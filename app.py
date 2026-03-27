f6e0aab30d33f2dc69650f3d5fd98dbe6a1982af
durklanez <durkjosue@gmail.com>
2026-03-27 22:04:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, redirect, session
 
-@app.route("/api/status")
-def status():
-    return jsonify({
-        "status": "online",
-        "app": "Inteligência do Cafunfo"
-    })
+app = Flask(__name__)
+app.secret_key = "cafunfo"
 
+users = {}
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['username']
+        pw = request.form['password']
+
+        if user in users and users[user] == pw:
+            session['user'] = user
+            return redirect('/')
+        else:
+            return "Login inválido"
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        user = request.form['username']
+        pw = request.form['password']
+
+        users[user] = pw
+        return redirect('/login')
+
+    return render_template('register.html')
